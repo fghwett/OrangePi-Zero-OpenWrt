2d5d903ee15f9b33441bfeead29685836c364362
阳阳打小就酷 <52075831+koyangyang@users.noreply.github.com>
2020-04-07 22:24:03 +0800

    Update OrangePi开发版.sh

--- OrangePi开发版.sh
+++ OrangePi开发版.sh
@@ -20,7 +20,7 @@ git clone https://github.com/melsem/openwrt-lede_xradio-xr819_soc-audio/blob/mas
 #git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic 
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb.git
 git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
-git clone https://github.com/project-openwrt/openwrt package/openwrt
+#git clone https://github.com/project-openwrt/openwrt package/openwrt
 #git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
 #mkdir -p package/parted && cp -i package/luci-app-diskman/Parted.Makefile package/parted/Makefile
 #git clone https://github.com/melsem/openwrt-lede_xradio-xr819_soc-audio/blob/master/packages_xradio-OpenWrt(Lede)-18.06.xx/add-patch_dts_file-wifi-xradio.patch package/add-patch_dts_file-wifi-xradio.patch
